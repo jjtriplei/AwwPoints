ddc15276459231126c6dcb866eeb9b6f7cda5ec0
JJTripleI <JJTripleI@gmail.com>
2016-08-14 13:53:21 -0700

    First commit

---
+++ Awwpoints.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from database import tables
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/news')
+def news():
+    my_user = "JJTRIPLEI"
+    some_array = [1, 15, 20, 25, 30, 35, 40]
+
+    # This is using JINJA2 to render the index.html file and passing it the above
+    # Variables.
+    return render_template('index.html',
+                           user=my_user,
+                           some_numbers=some_array)
+
+@app.route('/test')
+def testingJinja():
+    Joe_dic = tables.user_joe()
+    return render_template('test.html', dictionary=Joe_dic)
+
+
+# Only run this statement if someone directly runs this python file.
+# If it's being imported - this will not run.
+if __name__ == '__main__':
+    app.run()
+
+
---
+++ database/tables.py
@@ -0,0 +1,18 @@
+
+def create_fresh_tables():
+    print("Did I RUN?!?")
+    return
+
+
+def delete_all_tables():
+    print("Did I run?")
+    return
+
+
+def user_joe():
+    return {
+        "username": "Joseph Johnson",
+        "pic": "http://yahoo.com/jjtriplei.jpg",
+        "hatesPuppies": True,
+        "age": 123
+    }
