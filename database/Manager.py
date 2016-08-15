35e8b557f14acdc09ea88514b5a35182ded1eac2
JJTripleI <JJTripleI@gmail.com>
2016-08-15 13:28:06 -0700

    Continued work on DB Manager:
    - Fixed 'connect to DB' function
    - Refined 'Cursor' function

--- database/Manager.py
+++ database/Manager.py
@@ -3,16 +3,16 @@ import sqlite3
 
 #  Connects to DB file
 def get_db_connection():
-    conn = sqlite3.connect('/Development/Awwpoints/database/ap_db.sqlite')
+    conn = sqlite3.connect('ap_db.sqlite')
     return conn
 
 
 def get_db_cursor():
-    c = get_db_connection()
-    return c.cursor()
+    cursor = get_db_connection()
+    return cursor.cursor()
 
 
-c = get_db_cursor()
+cursor = get_db_cursor()
 
 # Save changes to DB with:
 # conn.commit()
