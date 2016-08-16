c59785f9fb3e064322a0e324441ffe0c915c750c
JJTripleI <JJTripleI@gmail.com>
2016-08-15 20:48:48 -0700

    Continued work on DB Manager:
    - Fixed 'connect to DB' function
    - - - Removed absolute file location. Replaced it with code that find or create the DB in the correct directory.

--- database/Manager.py
+++ database/Manager.py
@@ -1,15 +1,17 @@
 import sqlite3
+import os
+
 
 
 #  Connects to DB file
 def get_db_connection():
-    conn = sqlite3.connect('ap_db.sqlite')
+    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__),'ap_db.sqlite'))
     return conn
 
 
 def get_db_cursor():
-    cursor = get_db_connection()
-    return cursor.cursor()
+    db_connection = get_db_connection()
+    return db_connection.cursor()
 
 
 cursor = get_db_cursor()
@@ -19,3 +21,5 @@ cursor = get_db_cursor()
 
 # Close DB connection
 # conn.close()
+
+
